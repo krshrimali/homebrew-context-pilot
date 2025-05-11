beebc841d81bffdb2a1e435bca8736f8d0c4067f
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-05-11 17:00:30 +0530

    Rename

--- Formula/context-pilot.rb
+++ Formula/context-pilot.rb
@@ -1,4 +1,4 @@
-class V05 < Formula
+class ContextPilot < Formula
   desc "Get Context at your code cursor!"
   homepage "https://github.com/krshrimali/context-pilot-rs"
   url "https://github.com/krshrimali/context-pilot-rs/archive/refs/tags/v0.0.5.tar.gz"
