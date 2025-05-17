8ca9928cb7c35eb688a7a356697b8827a9613830
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2025-05-17 14:24:01 +0530

    v0.0.8 now

--- Formula/context-pilot.rb
+++ Formula/context-pilot.rb
@@ -1,8 +1,8 @@
 class ContextPilot < Formula
   desc "Get Context at your code cursor!"
   homepage "https://github.com/krshrimali/context-pilot-rs"
-  url "https://github.com/krshrimali/context-pilot-rs/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "de2a11f5d16a4820bd7b5810a39736c9d5d14352bf9f77bbf829f0e496eab228"
+  url "https://github.com/krshrimali/context-pilot-rs/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "880f797adeb31848f0acd405f8f35eb7313beda4e8dfc95ee88e90a8d44cd4ef"
   license "MIT"
 
   depends_on "rust" => :build
